268cd3b68a1e7fbfbf3574b0366bfe52e0ad257c
Eduardo Brandalise <brandalise.eduardo@gmail.com>
2023-05-24 23:43:40 +0200

    Before updating rolling movement logic.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -9,17 +9,20 @@ public class Player : MonoBehaviour
 {
     [SerializeField] private float FORWARDSPEED = 30f;
     [SerializeField] private float LATERALSPEED = 10f;
-    [SerializeField] private float ROLLSPEED = 10f;
+    [SerializeField] private float ROLLSPEED = 0.5f;
     [SerializeField] private float ROTATIONMAXANGLE = 30f;
+    [SerializeField] private float ROLLBACKSPEED = 10f;
     [SerializeField] private AnimationCurve rollbackAnimationCurve;
-    [SerializeField] private Transform planeModel;
+    [SerializeField] private Transform planeModelTransform;
     [SerializeField] private Transform propeller;
 
     private Quaternion initialRotation;
+    private float _rotationAngle;
+    private float _current, _target;
 
     private void Start()
     {
-        initialRotation = planeModel.rotation;
+        initialRotation = planeModelTransform.rotation;
     }
 
     private void FixedUpdate()
@@ -35,43 +38,43 @@ public class Player : MonoBehaviour
         var forwardMovement = Vector3.forward * FORWARDSPEED;
         // transform.position += ((Vector3.forward + new Vector3(lateralInput, 0, 0)) * SPEED) * Time.deltaTime;
         transform.position += (forwardMovement + lateralMovement) * Time.deltaTime;
-
         
         // ----------------- ROTATION MOVEMENT -----------------
-        
+
         if (lateralInput > 0)
         {
-            // Roll to the right.
-            planeModel.Rotate(0, 0, -ROLLSPEED);
-            if (planeModel.rotation.z < -ROTATIONMAXANGLE)
-            {
-                planeModel.rotation = new Quaternion(0, 0, -ROTATIONMAXANGLE, 0);
-            }
+            // Roll to the RIGHT.
+            _rotationAngle = -ROTATIONMAXANGLE;
+            _current = Mathf.MoveTowards(_current, _rotationAngle, ROLLSPEED * Time.deltaTime);
+            Vector3 rotationGoal = new Vector3(0, 0, _rotationAngle);
+            planeModelTransform.rotation = Quaternion.Lerp(Quaternion.Euler(rotationGoal), Quaternion.Euler(Vector3.zero), rollbackAnimationCurve.Evaluate(_current));
             return;
         }
         
         if (lateralInput < 0)
         {
-            // Roll to the left.
-            planeModel.Rotate(0, 0, ROLLSPEED);
+            // Roll to the LEFT.
+            _rotationAngle = ROTATIONMAXANGLE;
+            Vector3 rotationGoal = new Vector3(0, 0, _rotationAngle);
+            planeModelTransform.rotation = Quaternion.Lerp(Quaternion.Euler(rotationGoal), Quaternion.Euler(Vector3.zero), ROLLSPEED * Time.deltaTime);
             return;
         }
 
-        
         // ----------------- ROLLBACK MOVEMENT -----------------
    
         // Back to stable position.
-        var returnRollSpeed = 20f;
         var targetRotation = initialRotation;
-        var currentRotationAngle = new Quaternion(0, 0, planeModel.rotation.z, 0);
+        var rollbackMovement = ROLLBACKSPEED * Time.deltaTime;
+        
+        var currentRotationAngle = new Quaternion(0, 0, planeModelTransform.rotation.z, 0);
         // Mathf.MoveTowardsAngle()
         // planeModel.rotation = Quaternion.Slerp(currentRotationAngle, Quaternion.identity, Time.deltaTime * returnRollSpeed);
         // planeModel.rotation = Quaternion.Slerp(currentRotationAngle, Quaternion.identity, 0.6f);
-        planeModel.rotation = Quaternion.Slerp(planeModel.rotation, targetRotation, Time.deltaTime * returnRollSpeed);
-        if (Quaternion.Angle(planeModel.rotation, targetRotation) < 0.3f)
+        planeModelTransform.rotation = Quaternion.Slerp(planeModelTransform.rotation, targetRotation, Time.deltaTime * ROLLBACKSPEED);
+        if (Quaternion.Angle(planeModelTransform.rotation, targetRotation) < 0.3f)
         {
             // Stop the rotation and snap to the exact initial rotation
-            planeModel.rotation = targetRotation;
+            planeModelTransform.rotation = targetRotation;
         }
     }
 }
