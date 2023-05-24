616a2f3e03f1fc1759c9031c2c3d79f2c92d1f7f
Eduardo Brandalise <brandalise.eduardo@gmail.com>
2023-05-25 01:28:21 +0200

    New rolling movement.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,74 +7,84 @@ using UnityEngine.Serialization;
 
 public class Player : MonoBehaviour
 {
-    [SerializeField] private float FORWARDSPEED = 30f;
-    [SerializeField] private float LATERALSPEED = 10f;
-    [SerializeField] private float ROLLSPEED = 0.5f;
-    [SerializeField] private float ROTATIONMAXANGLE = 30f;
-    [SerializeField] private float ROLLBACKSPEED = 10f;
+    [SerializeField] private float forwardSpeed = 30f;
+    [SerializeField] private float lateralSpeed = 10f;
+    [SerializeField] private float rollSpeed = 0.5f;
+    [SerializeField] private float rotationMaxAngle = 30f;
+    [SerializeField] private float rollbackSpeed = 10f;
     [SerializeField] private AnimationCurve rollbackAnimationCurve;
     [SerializeField] private Transform planeModelTransform;
-    [SerializeField] private Transform propeller;
+    [SerializeField] private Transform propellerTransform;
 
-    private Quaternion initialRotation;
+    private Quaternion _currentRotationAngle;
     private float _rotationAngle;
     private float _current, _target;
-
-    private void Start()
-    {
-        initialRotation = planeModelTransform.rotation;
-    }
-
+    private float _propellerRotation = 50f;
+    
     private void FixedUpdate()
     {
         Move();
+        
+    }
+
+    private void LateUpdate()
+    {
+        RotatePropeller();
     }
 
     private void Move()
     {
         // ----------------- FORWARD MOVEMENT -----------------
         var lateralInput = InputManager.Instance.GetLateralMovementNormalized();
-        var lateralMovement = new Vector3(lateralInput, 0, 0) * LATERALSPEED;
-        var forwardMovement = Vector3.forward * FORWARDSPEED;
+        var lateralMovement = new Vector3(lateralInput, 0, 0) * lateralSpeed;
+        var forwardMovement = Vector3.forward * forwardSpeed;
         // transform.position += ((Vector3.forward + new Vector3(lateralInput, 0, 0)) * SPEED) * Time.deltaTime;
         transform.position += (forwardMovement + lateralMovement) * Time.deltaTime;
         
         // ----------------- ROTATION MOVEMENT -----------------
-
+        
         if (lateralInput > 0)
         {
             // Roll to the RIGHT.
-            _rotationAngle = -ROTATIONMAXANGLE;
-            _current = Mathf.MoveTowards(_current, _rotationAngle, ROLLSPEED * Time.deltaTime);
-            Vector3 rotationGoal = new Vector3(0, 0, _rotationAngle);
-            planeModelTransform.rotation = Quaternion.Lerp(Quaternion.Euler(rotationGoal), Quaternion.Euler(Vector3.zero), rollbackAnimationCurve.Evaluate(_current));
+            var targetRotationRight = Quaternion.Euler(new Vector3(0, 0, -rotationMaxAngle));
+            var rollMovement = rollSpeed * Time.deltaTime;
+            _currentRotationAngle = planeModelTransform.rotation;
+            
+            planeModelTransform.rotation = Quaternion.Slerp(_currentRotationAngle, targetRotationRight, rollMovement);
+            
             return;
         }
         
         if (lateralInput < 0)
         {
             // Roll to the LEFT.
-            _rotationAngle = ROTATIONMAXANGLE;
-            Vector3 rotationGoal = new Vector3(0, 0, _rotationAngle);
-            planeModelTransform.rotation = Quaternion.Lerp(Quaternion.Euler(rotationGoal), Quaternion.Euler(Vector3.zero), ROLLSPEED * Time.deltaTime);
+            var targetRotationLeft = Quaternion.Euler(new Vector3(0, 0, rotationMaxAngle));
+            var rollMovement = rollSpeed * Time.deltaTime;
+            _currentRotationAngle = planeModelTransform.rotation;
+            
+            planeModelTransform.rotation = Quaternion.Slerp(_currentRotationAngle, targetRotationLeft, rollMovement);
+            
             return;
         }
 
         // ----------------- ROLLBACK MOVEMENT -----------------
    
         // Back to stable position.
-        var targetRotation = initialRotation;
-        var rollbackMovement = ROLLBACKSPEED * Time.deltaTime;
+        var targetRotation = Quaternion.identity;
+        var rollbackMovement = rollbackSpeed * Time.deltaTime;
         
-        var currentRotationAngle = new Quaternion(0, 0, planeModelTransform.rotation.z, 0);
-        // Mathf.MoveTowardsAngle()
-        // planeModel.rotation = Quaternion.Slerp(currentRotationAngle, Quaternion.identity, Time.deltaTime * returnRollSpeed);
-        // planeModel.rotation = Quaternion.Slerp(currentRotationAngle, Quaternion.identity, 0.6f);
-        planeModelTransform.rotation = Quaternion.Slerp(planeModelTransform.rotation, targetRotation, Time.deltaTime * ROLLBACKSPEED);
+        _currentRotationAngle = new Quaternion(0, 0, planeModelTransform.rotation.z, 0);
+        planeModelTransform.rotation = Quaternion.Slerp(planeModelTransform.rotation, targetRotation, rollbackMovement);
         if (Quaternion.Angle(planeModelTransform.rotation, targetRotation) < 0.3f)
         {
             // Stop the rotation and snap to the exact initial rotation
             planeModelTransform.rotation = targetRotation;
         }
+    }
+
+    private void RotatePropeller()
+    {
+        _propellerRotation = (_propellerRotation + (forwardSpeed * _propellerRotation * Time.deltaTime)) % 360f;
+        propellerTransform.rotation = Quaternion.Euler(0, 0, _propellerRotation);
     }
 }
